ea9416c2e9f916c8069466f40739ffa650f5cd8f
Karina534 <smirnova7.04@mail.ru>
2025-09-19 23:05:13 +0300

    made admin and superuser

--- students/k3341/laboratory_works/Smirnova_Karina/Lab2/travelCompany/main/admin.py
+++ students/k3341/laboratory_works/Smirnova_Karina/Lab2/travelCompany/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tour, Reservation, Review
 
-# Register your models here.
+admin.site.register(Tour)
+admin.site.register(Reservation)
+admin.site.register(Review)
