fb8212c0fdcb121fb5698c4ae8e67684f87bdd44
Karina534 <smirnova7.04@mail.ru>
2025-09-19 22:59:30 +0300

    made migrations

--- students/k3341/laboratory_works/Smirnova_Karina/Lab2/travelCompany/users/models.py
+++ students/k3341/laboratory_works/Smirnova_Karina/Lab2/travelCompany/users/models.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from students.k3341.laboratory_works.Smirnova_Karina.Lab2.travelCompany.main.models import Tour
-
 
 class User(AbstractUser):
-    reserved_tours = models.ManyToManyField(Tour, through='Reservation')
+    reserved_tours = models.ManyToManyField('main.Tour', through='main.Reservation')
 
     def __str__(self):
         return self.username
