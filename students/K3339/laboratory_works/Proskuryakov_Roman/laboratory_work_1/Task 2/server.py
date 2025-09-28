5dfa9136d9f33a65b3ccbdb7a85e99c635033c84
Roman_Proskuryakov <2romanio005@gmail.com>
2025-09-28 14:29:26 +0300

    [*] Task 2 подправил

--- students/K3339/laboratory_works/Proskuryakov_Roman/laboratory_work_1/Task 2/server.py
+++ students/K3339/laboratory_works/Proskuryakov_Roman/laboratory_work_1/Task 2/server.py
@@ -63,7 +63,6 @@ threads=[]
 
 def run_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen()
         print(f"Server listening on {HOST}:{PORT}")
