3ed306528a55032f683b4ec2cc0f132a980e738c
Artem <artem00561@gmail.com>
2025-09-29 20:56:32 +0300

    feat(conference): Добавлен ConferenceListView и ConferenceDetailView, а также шаблоны для них

--- students/k3339/Gordeev_Artem/Lr_2/conference_project/conferences/urls.py
+++ students/k3339/Gordeev_Artem/Lr_2/conference_project/conferences/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, ConferenceListView, ConferenceDetailView
 
 urlpatterns = [
+    # Страницы аутентификации
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
+
+    # Страницы конференций
+    path('', ConferenceListView.as_view(), name='conference_list'),
+    path('conference/<int:pk>/', ConferenceDetailView.as_view(), name='conference_detail'),
 ]
--- students/k3339/Gordeev_Artem/Lr_2/conference_project/conferences/views.py
+++ students/k3339/Gordeev_Artem/Lr_2/conference_project/conferences/views.py
@@ -1,9 +1,35 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views.generic import ListView, DetailView
+
+from .models import Conference
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class ConferenceListView(ListView):
+    model = Conference
+    template_name = 'conferences/conference_list.html'
+    context_object_name = 'conferences'  # Имя переменной в шаблоне
+    paginate_by = 3
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) | Q(description__icontains=query)
+            )
+        return queryset
+
+
+class ConferenceDetailView(DetailView):
+    model = Conference
+    template_name = 'conferences/conference_detail.html'
+    context_object_name = 'conference'
