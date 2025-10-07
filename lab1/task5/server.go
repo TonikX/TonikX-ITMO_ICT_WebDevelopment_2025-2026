8f8c33626595dff97fd1358f6f0861712745d777
Vladimir Stabrovsky <ethereal.sent@gmail.com>
2025-10-07 15:53:48 +0300

    upd

--- lab1/task5/server.go
+++ lab1/task5/server.go
@@ -86,7 +86,7 @@ func getStats(w http.ResponseWriter, r *http.Request) {
 }
 
 func addStat(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
+	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
