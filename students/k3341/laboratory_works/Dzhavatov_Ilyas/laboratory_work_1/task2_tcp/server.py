0069a5467675fd4f8f13c30d588a5770c4ffa238
ilyaugustt <118938328+ilyaugustt@users.noreply.github.com>
2025-10-10 02:41:54 +0300

    Update server.py

--- students/k3341/laboratory_works/Dzhavatov_Ilyas/laboratory_work_1/task2_tcp/server.py
+++ students/k3341/laboratory_works/Dzhavatov_Ilyas/laboratory_work_1/task2_tcp/server.py
@@ -1,8 +1,8 @@
 # server.py
 import socket
 
-HOST = '127.0.0.1'   # локальный адрес (для теста)
-PORT = 5000          # любой свободный порт
+HOST = '127.0.0.1'   
+PORT = 5000          
 
 def calculate_area(base: float, height: float) -> float:
     """Формула площади параллелограмма: S = a * h"""
@@ -12,27 +12,27 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((HOST, PORT))
-    sock.listen(1)  # максимум 1 соединение в очереди
+    sock.listen(1)  
     print(f"TCP server listening on {HOST}:{PORT}...")
 
     conn, addr = sock.accept()
     print(f"Connected by {addr}")
 
     with conn:
-        # получаем данные от клиента
+        
         data = conn.recv(1024).decode('utf-8')
         if not data:
             return
 
         print(f"Received from client: {data}")
         try:
-            base, height = map(float, data.split())  # строка → числа
+            base, height = map(float, data.split())  
             area = calculate_area(base, height)
             response = f"Площадь параллелограмма = {area}"
         except Exception as e:
             response = f"Ошибка вычисления: {e}"
 
-        # отправляем результат клиенту
+        
         conn.sendall(response.encode('utf-8'))
         print(f"Sent to client: {response}")
 
