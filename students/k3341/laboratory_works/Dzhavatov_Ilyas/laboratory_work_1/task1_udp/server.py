0aa8b33c34a099fa3328d36628a7c713e8c8a254
ilyaugustt <118938328+ilyaugustt@users.noreply.github.com>
2025-10-10 02:41:02 +0300

    Update server.py

--- students/k3341/laboratory_works/Dzhavatov_Ilyas/laboratory_work_1/task1_udp/server.py
+++ students/k3341/laboratory_works/Dzhavatov_Ilyas/laboratory_work_1/task1_udp/server.py
@@ -1,22 +1,22 @@
 # server.py
 import socket
 
-HOST = '127.0.0.1'   # адрес для локального теста. Если нужен внешний доступ, используйте '0.0.0.0' или реальный IP.
-PORT = 9999          # порт (можно изменить)
+HOST = '127.0.0.1'   
+PORT = 9999          
 
 def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP -> SOCK_DGRAM
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((HOST, PORT))
     print(f'UDP server listening on {HOST}:{PORT} ...')
 
     try:
         while True:
-            data, addr = sock.recvfrom(1024)  # буфер 1024 байта
+            data, addr = sock.recvfrom(1024)  
             msg = data.decode('utf-8')
             print(f'Received from {addr}: {msg}')
 
-            # Ответ клиенту
+            
             if msg == 'Hello, server':
                 reply = 'Hello, client'
             else:
