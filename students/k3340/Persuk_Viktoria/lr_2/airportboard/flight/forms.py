b739903462badf40b1e81cf2fed51741b2188817
Viktoria <v.p.persuk@niuitmo.ru>
2025-11-02 23:29:56 +0300

    feat: add forms

---
+++ students/k3340/Persuk_Viktoria/lr_2/airportboard/flight/forms.py
@@ -0,0 +1,65 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Reservation, Comment
+
+
+class ReservationForm(forms.ModelForm):
+    '''
+    Форма бронирования рейса
+    '''
+    class Meta:
+        model = Reservation
+        fields = ['seat_number']
+        widgets = {
+            'seat_number': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите номер места',
+            })
+        }
+
+
+class CommentForm(forms.ModelForm):
+    '''
+    Форма комментариев
+    '''
+    class Meta:
+        model = Comment
+        fields = ['text', 'rating']
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Напишите ваш отзыв...',
+            }),
+            'rating': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'min': 1,
+                'max': 10,
+                'placeholder': 'Рейтинг от 1 до 10',
+            }),
+        }
+
+
+class RegisterForms(UserCreationForm):
+    '''
+    Форма регистрации
+    '''
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите ваш email',
+        })
+    )
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
