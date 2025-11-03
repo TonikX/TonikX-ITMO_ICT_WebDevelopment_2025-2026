3ff102e4171adc7c86c2cc85b657362a28c970f0
Viktoria <v.p.persuk@niuitmo.ru>
2025-11-03 21:31:15 +0300

    feat: add new views and urls

--- students/k3340/Persuk_Viktoria/lr_2/airportboard/flight/urls.py
+++ students/k3340/Persuk_Viktoria/lr_2/airportboard/flight/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('flight/<int:flight_id>', views.flight_detail, name='flight_detail'),
     path('flight/<int:flight_id>/reserve/', views.create_reservation, name='create_reservation'),
     path('flight/<int:flight_id>/comment/', views.add_comment, name='add_comment'),
+    path('reservation/<int:pk>/edit', views.reservation_update, name='reservation_update'),
+    path('reservation/<int:pk>/delete', views.reservation_delete, name='reservation_delete'),
+    path('my-reservations/', views.my_reservations, name='my_reservations')
 ]
--- students/k3340/Persuk_Viktoria/lr_2/airportboard/flight/views.py
+++ students/k3340/Persuk_Viktoria/lr_2/airportboard/flight/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from shapely import is_valid
 from .forms import ReservationForm, CommentForm, RegisterForm
@@ -92,3 +94,65 @@ def flight_detail(request: HttpRequest, flight_id: int) -> HttpResponse:
         'reservations': reservations,
         'comments': comments,
     })
+
+
+@login_required
+def reservation_update(request: HttpRequest, pk: int) -> HttpResponse:
+    '''
+    Редактирование бронирования
+    '''
+    reservation = get_object_or_404(Reservation, pk=pk)
+
+    # проверка прав
+    if request.user != reservation.user and not request.user.is_staff:
+        messages.error(request, 'Вы не можете редактировать это бронирование.')
+        return redirect('my_reservations')
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST, instance=reservation)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Бронирование успешно обновлено.')
+            return redirect('my_reservations')
+    else:
+        form = ReservationForm(instance=reservation)
+
+    return render(request, 'flight/reservation_form.html', {
+        'form': form,
+        'edit_mode': True,
+    })
+
+
+@login_required
+def reservation_delete(request: HttpRequest, pk: int) -> HttpResponse:
+    '''
+    Удаление бронирования
+    '''
+    reservation = get_object_or_404(Reservation, pk=pk)
+
+    # проверка прав
+    if request.user != reservation.user and not request.user.is_staff:
+        messages.error(request, 'Вы не можете удалить это бронирование.')
+        return redirect('my_reservations')
+
+    if request.method == 'POST':
+        reservation.delete()
+        messages.success(request, 'Бронирование удалено.')
+        return redirect('my_reservations')
+
+    return render(request, 'flight/reservation_confirm_delete.html', {'reservation': reservation})
+
+
+@login_required
+def my_reservations(request: HttpRequest) -> HttpResponse:
+    '''
+    Просмотр бронирований авторизованного пользователя + пагинация
+    '''
+    reservations = Reservation.objects.filter(user=request.user).select_related('flight').order_by('-created_at')
+
+    # пагинация
+    pagination = Paginator(reservations, 10)
+    page_number = request.GET.get('page')
+    page_obj = pagination.get_page(page_number)
+
+    return render(request, 'flight/my_reservations.html', {'page_obj': page_obj})
