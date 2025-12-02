3866086b586ab63ec9ad9321bc278260d57e1767
Viktoria <v.p.persuk@niuitmo.ru>
2025-12-02 03:15:33 +0300

    feat: add urls and views for drones

--- students/k3340/Persuk_Viktoria/lr_3/laboratory_work/backend/urls.py
+++ students/k3340/Persuk_Viktoria/lr_3/laboratory_work/backend/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    re_path(r'^auth/', include('djoser.urls')),
-]
+    # Auth endpoints (Djoser + JWT)
+    re_path(r"^auth/", include("djoser.urls")),
+    re_path(r"^auth/", include("djoser.urls.jwt")),
+    # Domain APIs
+    path("api/", include(("accounts.urls", "accounts"), namespace="accounts")),
+    path("api/", include(("drones.urls", "drones"), namespace="drones")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- students/k3340/Persuk_Viktoria/lr_3/laboratory_work/drones/views.py
+++ students/k3340/Persuk_Viktoria/lr_3/laboratory_work/drones/views.py
@@ -1,3 +1,128 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import Drones, Flights, FlightLogs, Documents
+from .serializer import DroneSerializer, FlightSerializer, FlightLogSerializer, DocumentSerializer
+
+
+class AuthenticatedModelViewSet(viewsets.ModelViewSet):
+    """
+    Базовый класс, который требует аутентификацию на каждом эндпоинте
+    """
+
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class DroneViewSet(AuthenticatedModelViewSet):
+    """
+    CRUD операции для дронов
+    Поддерживает вложенный доступ через /drones/<id>/flights/ и /drones/<id>/documents/
+    """
+
+    serializer_class = DroneSerializer
+
+
+    def get_queryset(self):
+        """
+        Получение списка дронов
+        """
+        return (
+            Drones.objects.all()
+            .prefetch_related("flights", "documents")
+            .order_by("id")
+        )
+
+
+class FlightViewSet(AuthenticatedModelViewSet):
+    """
+    CRUD операции для полётов
+    Поддерживает фильтрацию по дрону при использовании вложенного роутера
+    """
+
+    serializer_class = FlightSerializer
+
+
+    def get_queryset(self):
+        """
+        Получение списка полётов для дрона
+        """
+        queryset = Flights.objects.select_related("drone_id").prefetch_related("logs").order_by("-start_datetime")
+        drone_pk = self.kwargs.get("drone_pk")
+        if drone_pk:
+            queryset = queryset.filter(drone_id_id=drone_pk)
+        return queryset
+
+
+    def perform_create(self, serializer):
+        """
+        Создание полёта для дрона
+        """
+        drone_pk = self.kwargs.get("drone_pk")
+        if drone_pk:
+            drone = get_object_or_404(Drones, pk=drone_pk)
+            serializer.save(drone_id=drone)
+        else:
+            serializer.save()
+
+
+class FlightLogViewSet(AuthenticatedModelViewSet):
+    """
+    CRUD операции для журналов полётов
+    Поддерживает вложенный доступ через /flights/<id>/logs/
+    """
+
+    serializer_class = FlightLogSerializer
+
+
+    def get_queryset(self):
+        """
+        Получение списка журналов полётов для дрона
+        """
+        queryset = FlightLogs.objects.select_related("flight_id", "flight_id__drone_id").order_by("-timestamp")
+        flight_pk = self.kwargs.get("flight_pk")
+        if flight_pk:
+            queryset = queryset.filter(flight_id_id=flight_pk)
+        return queryset
+
+
+    def perform_create(self, serializer):
+        """
+        Создание журнала полёта
+        """
+        flight_pk = self.kwargs.get("flight_pk")
+        if flight_pk:
+            flight = get_object_or_404(Flights, pk=flight_pk)
+            serializer.save(flight_id=flight)
+        else:
+            serializer.save()
+
+
+class DocumentViewSet(AuthenticatedModelViewSet):
+    """
+    CRUD операции для документов
+    Поддерживает вложенный доступ через /drones/<id>/documents/
+    """
+
+    serializer_class = DocumentSerializer
+
+    def get_queryset(self):
+        """
+        Получение списка документов для дрона
+        """
+        queryset = Documents.objects.select_related("drone_id").order_by("-uploaded_at")
+        drone_pk = self.kwargs.get("drone_pk")
+        if drone_pk:
+            queryset = queryset.filter(drone_id_id=drone_pk)
+        return queryset
+
+
+    def perform_create(self, serializer):
+        """
+        Создание документа для дрона
+        """
+        drone_pk = self.kwargs.get("drone_pk")
+        if drone_pk:
+            drone = get_object_or_404(Drones, pk=drone_pk)
+            serializer.save(drone_id=drone)
+        else:
+            serializer.save()
