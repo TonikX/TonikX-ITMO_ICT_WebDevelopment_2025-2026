a6ab6bcbc6b2f4add0391fe26a7aebb4db6e9f62
Cheroit <maksimermakov1337@yandex.ru>
2025-12-08 03:16:53 +0300

    lab4 done

---
+++ students/k3340/Ermakov_Maxim/lr4/airport_frontend/src/api/api.js
@@ -0,0 +1,17 @@
+// src/api/api.js
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000", // адрес твоего Django-сервера
+});
+
+// Перед каждым запросом подставляем токен, если он есть
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("auth_token");
+  if (token) {
+    config.headers.Authorization = `Token ${token}`;
+  }
+  return config;
+});
+
+export default api;
