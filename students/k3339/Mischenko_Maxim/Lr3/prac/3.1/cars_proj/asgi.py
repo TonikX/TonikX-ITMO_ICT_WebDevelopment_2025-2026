bb2dd2dacc3652c76deb2acedb20ccd7e05673df
Maxim Mischenko <maxim.y.mischenko@gmail.com>
2025-12-10 19:13:14 +0300

    add prac 3.1

--- students/k3339/Mischenko_Maxim/Lr3/prac/3.1/cars_proj/asgi.py
+++ students/k3339/Mischenko_Maxim/Lr3/prac/3.1/cars_proj/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for car_system project.
+ASGI config for cars_proj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cars_proj.settings')
 
 application = get_asgi_application()
