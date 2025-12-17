02726a5f3779194ad05e8d17d84297c3696669bf
f0x1d <f0x1d@yandex-team.ru>
2025-12-17 23:15:24 +0300

    added task1

---
+++ students/k3339/Zoteev_Maksim/lab4/vue-project/src/views/Warriors.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="app">
+      <h1>Портал информации о войнах в онлайн РПГ</h1>
+      <button v-on:click="fetchWarriors">Получить список войнов</button>
+      <warrior-form/>
+      <warrior-list
+          v-bind:warriors="warriors"
+      />
+    </div>
+</template>
+
+<script>
+import WarriorForm from "@/components/WarriorForm.vue";
+import WarriorList from "@/components/WarriorList.vue";
+import axios from "axios";
+
+export default {
+  components: {
+    WarriorForm, WarriorList
+  },
+
+  data() {
+    return {
+      warriors: [],
+    }
+  },
+  methods: {
+    async fetchWarriors () {
+      try {
+        const response = await axios.get('http://62.109.28.95:8890/warriors/list/')
+        console.log(response.data.results)
+        this.warriors = response.data.results
+      } catch (e) {
+        alert('Ошибка')
+      }
+    }
+
+  },
+  mounted() {
+    this.fetchWarriors()
+  }
+}
+</script>
+
