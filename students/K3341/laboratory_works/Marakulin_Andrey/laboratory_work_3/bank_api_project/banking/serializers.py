2a483ee9d2450c1fd4dad641bf1f2e47d6e3c28a
MrMegnis <mr.megnis@mail.ru>
2025-12-24 17:59:22 +0300

    add buisens logic

--- students/K3341/laboratory_works/Marakulin_Andrey/laboratory_work_3/bank_api_project/banking/serializers.py
+++ students/K3341/laboratory_works/Marakulin_Andrey/laboratory_work_3/bank_api_project/banking/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import *
-
+from datetime import date
 
 
 class AccrualScheduleSerializer(serializers.ModelSerializer):
@@ -20,6 +20,11 @@ class PassportSerializer(serializers.ModelSerializer):
         model = Passport
         fields = '__all__'
 
+    def validate_date_issue(self, value):
+        if value > date.today():
+            raise serializers.ValidationError("Дата выдачи паспорта не может быть в будущем!")
+        return value
+
 
 
 class ClientSerializer(serializers.ModelSerializer):
@@ -37,6 +42,26 @@ class DepositSerializer(serializers.ModelSerializer):
         model = Deposit
         fields = '__all__'
 
+    def validate(self, data):
+        deposit_type = data.get('deposit_type')
+        deposit_sum = data.get('deposit_sum')
+        start_date = data.get('deposit_date')
+        end_date = data.get('return_date')
+
+        if deposit_type and deposit_sum:
+            if deposit_sum < deposit_type.min_sum:
+                raise serializers.ValidationError({
+                    "deposit_sum": f"Сумма меньше минимальной ({deposit_type.min_sum}) для тарифа '{deposit_type.name}'"
+                })
+
+        if start_date and end_date:
+            if end_date <= start_date:
+                raise serializers.ValidationError({
+                    "return_date": "Дата возврата должна быть позже даты открытия вклада."
+                })
+
+        return data
+
 
 class LoanSerializer(serializers.ModelSerializer):
     payouts = PayoutScheduleSerializer(many=True, read_only=True)
@@ -44,6 +69,26 @@ class LoanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Loan
         fields = '__all__'
+
+    def validate(self, data):
+        date_issue = data.get('date_issue')
+        close_date = data.get('close_date')
+        sum_credit = data.get('sum_credit')
+        monthly_payment = data.get('monthly_payment')
+
+        if date_issue and close_date:
+            if close_date <= date_issue:
+                raise serializers.ValidationError({
+                    "close_date": "Дата закрытия кредита должна быть строго позже даты выдачи."
+                })
+
+        if sum_credit and monthly_payment:
+            if monthly_payment > sum_credit:
+                raise serializers.ValidationError({
+                    "monthly_payment": "Ежемесячный платеж не может превышать сумму кредита."
+                })
+
+        return data
 
 
 
