4373a6353ef1b0b825ad55df2a8d669300c7d227
Roman_Proskuryakov <2romanio005@gmail.com>
2025-12-26 21:03:27 +0300

    [+] add start models

--- students/K3339/laboratory_works/Proskuryakov_Roman/laboratory_work_3/refueling_project/refueling_app/models.py
+++ students/K3339/laboratory_works/Proskuryakov_Roman/laboratory_work_3/refueling_project/refueling_app/models.py
@@ -1,3 +1,139 @@
 from django.db import models
 
-# Create your models here.
+class FuelReference(models.Model):
+    id_kind_fuel = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=40)
+    burning_temp = models.IntegerField()
+    brand = models.CharField(max_length=40)
+    density = models.IntegerField()
+    season = models.IntegerField()
+    percent_sulfur = models.IntegerField()
+    min_usage_temp = models.IntegerField()
+
+    class Meta:
+        db_table = "fuel_reference"
+
+
+class Companies(models.Model):
+    id_company = models.AutoField(primary_key=True)
+    type_company = models.IntegerField()
+    legal_address = models.CharField(max_length=100)
+    company_title = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "companies"
+
+
+class ProducedFuel(models.Model):
+    id_produced_fuel = models.AutoField(primary_key=True)
+    id_kind_fuel = models.ForeignKey(
+        FuelReference,
+        on_delete=models.CASCADE,
+        db_column="id_kind_fuel"
+    )
+    id_company = models.ForeignKey(
+        Companies,
+        on_delete=models.CASCADE,
+        db_column="id_company"
+    )
+
+    class Meta:
+        db_table = "produced_fuel"
+
+
+class GasStation(models.Model):
+    id_station = models.AutoField(primary_key=True)
+    id_company = models.ForeignKey(
+        Companies,
+        on_delete=models.CASCADE,
+        db_column="id_company"
+    )
+    station_address = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "gas_station"
+
+
+class SoldFuel(models.Model):
+    id_sold_fuel = models.AutoField(primary_key=True)
+    id_produced_fuel = models.ForeignKey(
+        ProducedFuel,
+        on_delete=models.CASCADE,
+        db_column="id_produced_fuel"
+    )
+    id_station = models.ForeignKey(
+        GasStation,
+        on_delete=models.CASCADE,
+        db_column="id_station"
+    )
+
+    class Meta:
+        db_table = "sold_fuel"
+
+
+class FuelPrices(models.Model):
+    id_fuel_price = models.AutoField(primary_key=True)
+    id_sold_fuel = models.ForeignKey(
+        SoldFuel,
+        on_delete=models.CASCADE,
+        db_column="id_sold_fuel"
+    )
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(null=True, blank=True)
+    per_liter = models.IntegerField()
+
+    class Meta:
+        db_table = "fuel_prices"
+
+
+class Clients(models.Model):
+    id_client = models.AutoField(primary_key=True)
+    surname = models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
+    patronymic = models.CharField(max_length=40)
+    phone_number = models.BigIntegerField()
+    address = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "clients"
+
+
+class ClientCards(models.Model):
+    id_card = models.AutoField(primary_key=True)
+    id_company = models.ForeignKey(
+        Companies,
+        on_delete=models.CASCADE,
+        db_column="id_company"
+    )
+    id_client = models.ForeignKey(
+        Clients,
+        on_delete=models.CASCADE,
+        db_column="id_client"
+    )
+    start_date = models.DateField()
+    end_date = models.DateField()
+    balance = models.IntegerField()
+    discount_percent = models.IntegerField()
+    discount_rub = models.IntegerField()
+
+    class Meta:
+        db_table = "client_cards"
+
+
+class Sales(models.Model):
+    id_sales = models.BigAutoField(primary_key=True)
+    id_fuel_price = models.ForeignKey(
+        FuelPrices,
+        on_delete=models.CASCADE,
+        db_column="id_fuel_price"
+    )
+    id_card = models.ForeignKey(
+        ClientCards,
+        on_delete=models.CASCADE,
+        db_column="id_card"
+    )
+    sale_date = models.DateTimeField()
+    sold_liters_volume = models.IntegerField()
+
+    class Meta:
+        db_table = "sales"
