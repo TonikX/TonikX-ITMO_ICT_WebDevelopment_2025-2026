2fb5eb68803e91abbf86033252a1858d55a797fc
Manfred512 <121448574+Manfred512@users.noreply.github.com>
2026-03-05 15:59:06 +0300

    add all the labs

--- students/K3339/Mitrofanov_Georgy/Lab2/homework_board/settings.py
+++ students/K3339/Mitrofanov_Georgy/Lab2/homework_board/settings.py
@@ -52,7 +52,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "lr2",
         "USER": "postgres",
-        "PASSWORD": "",
+        "PASSWORD": "A1111_gosha",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
