eef785b15c3a408a308884d0e67a8e5fb5f800b4
vovasobol1 <103885272+vovasobol1@users.noreply.github.com>
2026-04-13 20:40:36 +0300

    done

---
+++ students/k3340/Sobol_Vladimir/lab2_sem2/task2/urls.py
@@ -0,0 +1,15 @@
+"""Список URL для параллельного парсинга."""
+URLS = [
+    "https://example.com/",
+    "https://www.python.org/",
+    "https://docs.python.org/3/",
+    "https://pypi.org/",
+    "https://en.wikipedia.org/wiki/Python_(programming_language)",
+    "https://en.wikipedia.org/wiki/Concurrency_(computer_science)",
+    "https://en.wikipedia.org/wiki/Multiprocessing",
+    "https://en.wikipedia.org/wiki/Thread_(computing)",
+    "https://en.wikipedia.org/wiki/Asynchronous_I/O",
+    "https://en.wikipedia.org/wiki/Global_interpreter_lock",
+    "https://fastapi.tiangolo.com/",
+    "https://www.djangoproject.com/",
+]
